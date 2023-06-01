792657c5946a732b3132214fb0be143a4f0e93dd
CynGL <cyntialino09@gmail.com>
2023-06-01 20:16:27 -0300

    MeRecicla

---
+++ app/src/main/java/br/com/etecia/merecicla/SplashActivity.java
@@ -0,0 +1,27 @@
+package br.com.etecia.merecicla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        //criando o carregamento da janela de login
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        LoginActivity.class));
+                finish();//fechando a janela atual
+            }
+        },3000);
+    }
+}
+
