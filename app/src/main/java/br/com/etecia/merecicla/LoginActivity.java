2f943cbe39bc7a86acf27b8ed681ff1ac3d87028
CynGL <cyntialino09@gmail.com>
2023-06-01 21:55:22 -0300

    MeRecicla

--- app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
+++ app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
@@ -3,12 +3,17 @@ package br.com.etecia.merecicla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+
     }
 }
